a4171e793cb02d223505a436c51a89ffa72861e2
Axel Frau <axel.frau.tr@gmail.com>
2019-02-05 21:12:59 +0100

    update listened port for heroku

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
 const data = require('./parc-des-installations-de-production.json')
-
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 app.use('/', express.static('public'))
 
 app.get('/api', function (req, res) {
     res.json(data);
   })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port)
 })
